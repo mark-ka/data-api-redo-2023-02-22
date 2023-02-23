92cf09e45f128976c82a8dec3e059018e572569c
Markus Kramer <96992448+mark-ka@users.noreply.github.com>
2023-02-23 16:10:48 +0100

    lewagon.proxy doesnt work anymore. need to make a SIGNUP

--- weather.py
+++ weather.py
@@ -7,20 +7,27 @@ import requests
 BASE_URI = "https://weather.lewagon.com"
 
 
+def weather_forecast(lat, lon):
+    '''Return a 5-day weather forecast for the city, given its latitude and longitude.'''
+    url = f"https://weather.lewagon.com/2.5/forecast/daily?lat={lat}&lon={lon}cnt=5"
+
 def search_city(query):
     '''
     Look for a given city. If multiple options are returned, have the user choose between them.
     Return one city (or None)
     '''
-    pass  # YOUR CODE HERE
-
-def weather_forecast(lat, lon):
-    '''Return a 5-day weather forecast for the city, given its latitude and longitude.'''
-    pass  # YOUR CODE HERE
+    url = f"https://weather.lewagon.com/geo/1.0/direct?q={query}"
+    response = requests.get(url).json()
+    city = response[0]
+    if query.capitalize() == city['name']:
+        print(f"Here's the weather in {city['name']}")
+        weather_forecast(city['lat'], city['lon'])
+    else:
+        main()
 
 def main():
     '''Ask user for a city and display weather forecast'''
-    query = input("City?\n> ")
+    query = input("Good Day master! How can I serve you? Please enter a city name.\n> ")
     city = search_city(query)
 
     # TODO: Display weather forecast for a given city
